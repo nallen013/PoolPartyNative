1bea4aa1c0792089619bc349bf303e7a4777e63c
Nicholas Allen <allen.2020@osu.edu>
2019-06-05 23:37:56 -0400

    Removed labels from FirstViewController

--- PoolParty/FirstViewController.swift
+++ PoolParty/FirstViewController.swift
@@ -9,16 +9,9 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-    @IBOutlet weak var NameLabel: UILabel!
-    @IBOutlet weak var EmailLabel: UILabel!
-    @IBOutlet weak var PhoneLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        NameLabel.text = UserDefaults.standard.string(forKey: "name")
-        EmailLabel.text = UserDefaults.standard.string(forKey: "email")
-        PhoneLabel.text = UserDefaults.standard.string(forKey: "phone")
     }
 }
 
