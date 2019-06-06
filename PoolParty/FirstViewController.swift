c22705ea5824543920008a1fb71afb0ea4704a12
Nicholas Allen <allen.2020@osu.edu>
2019-06-05 22:32:01 -0400

    Initial upload of XCode project

---
+++ PoolParty/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PoolParty
+//
+//  Created by Nicholas Allen on 6/5/19.
+//  Copyright © 2019 Allen Application Design. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PoolParty/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PoolParty
+//
+//  Created by Nicholas Allen on 6/5/19.
+//  Copyright © 2019 Allen Application Design. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
