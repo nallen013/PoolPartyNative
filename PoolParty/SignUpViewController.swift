5765b411d9df2b4bbd45c0717bc065ab169aebdc
Nicholas Allen <allen.2020@osu.edu>
2019-06-05 23:08:38 -0400

    Created sign-up screen. Text boxes do not function at all yet.

---
+++ PoolParty/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  PoolParty
+//
+//  Created by Nicholas Allen on 6/5/19.
+//  Copyright © 2019 Allen Application Design. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
